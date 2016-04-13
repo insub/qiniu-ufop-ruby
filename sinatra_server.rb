dc4521bbd0bb9a3deb2538485727eec4b784903b
Insub <insub@126.com>
2016-04-13 21:04:17 +0800

    Update sinatra_server.rb

--- sinatra_server.rb
+++ sinatra_server.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/ruby
 require 'sinatra'
+require 'json'
 
 set :server, %w[thin mongrel webrick]
 set :environment, :production
 set :port, 9100
 
 post '/uop' do
-  'Hello world!'
+  params = JSON.parse(request.body.read)
+  "Hello world! url:#{params['src']['url']}"
 end
